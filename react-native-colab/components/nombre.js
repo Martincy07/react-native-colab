53aa6f5776cffb97ec7e81c6ed21d234e8833640
MARTIN EITAN CYWINER <48313358@ort>
2025-08-22 10:18:00 -0300

    hola

---
+++ react-native-colab/components/nombre.js
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Nnombre() {
+  return (
+    <View>
+      <Text style={{ fontSize: 20 }}>Martin Cywiner</Text>
+    </View>
+  );
+}
